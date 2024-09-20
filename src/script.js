6dd44d160602fe15bd5846b5adcccd83f153eb13
fiamanillah <fiamanillahb2@gmail.com>
2024-09-21 04:12:09 +0600

    Coupon and half of seat section added

---
+++ src/script.js
@@ -0,0 +1,11 @@
+// const seats = document.querySelectorAll("#seats button");
+
+// console.log(seats);
+
+// seats.forEach(seat => {
+//     seat.addEventListener("click", (e)=>{
+//         e.target.classList.add("btn-active")
+//         console.log(e.target);
+        
+//     })
+// })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        // Add more custom font families as needed
+      },
+      colors:{
+        primary : "#1DD100",
+        primary10 : "#1DD1001A",
+        dullText : "#030712B3"
+      },
+      boxShadow : {
+        primary : " 0px 3px 0px 0px #1DD100"
+      },
+      gridTemplateColumns: {
+        // Custom grid template: 1st col 50px, other 5 cols 1fr each
+        'custom-layout': '50px repeat(5, 1fr)',
+      },
+    },
   },
   plugins: [],
 }
